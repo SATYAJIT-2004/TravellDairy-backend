7054780f2534757953cde6094c6ae3a5609f3457
SATYAJIT-2004 <satyajit2568@gmail.com>
2026-01-11 14:02:30 +0530

    / route added

--- index.js
+++ index.js
@@ -30,6 +30,11 @@ app.use(cookieParser())
 
 app.use(express.json())
 
+app.get("/", (req, res) => {
+  res.status(200).send("Travell Dairy Backend is running...");
+});
+
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT,()=>{
     console.log("Server is running on port: ${PORT}")
